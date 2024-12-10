e12f9ec7dacf58b147a93402a1ba8c62a0cc332f
Nick Robinson <nrobinson13@gmail.com>
2024-12-10 05:35:50 -0500

    add day 9

--- src/day_9/main.rs
+++ src/day_9/main.rs
@@ -1,12 +1,15 @@
 fn main() {
-    let input = "12345";
+    let input = "2333133121414131402";
     let blocks = get_blocks(input);
-    println!("Blocks: {}", blocks);
+    println!("Blocks: {:?}", blocks);
+    let compressed = compress_blocks(blocks);
+    println!("Compressed Blocks: {:?}", compressed);
+    println!("Checksum: {}", compute_checksum(compressed));
 }
 
-fn get_blocks(diskmap: &str) -> String {
+fn get_blocks(diskmap: &str) -> Vec<String> {
     let mut i = 0;
-    let mut blocks = String::new();
+    let mut blocks: Vec<String> = Vec::new();
     let mut current_id = 0;
     while i < diskmap.len() {
         // Determine the length of the slice: 2 if there are at least 2 chars left, otherwise 1.
@@ -19,10 +22,10 @@ fn get_blocks(diskmap: &str) -> String {
         };
 
         for _ in 0..file_count.unwrap() {
-            blocks.push_str(&current_id.to_string());
+            blocks.push(current_id.to_string());
         }
         for _ in 0..free_space {
-            blocks.push('.');
+            blocks.push(".".to_string());
         }
 
         i += slice_len;
@@ -32,19 +35,78 @@ fn get_blocks(diskmap: &str) -> String {
     blocks
 }
 
+fn compute_checksum(compressed_blocks: Vec<String>) -> i64 {
+    let mut checksum: i64 = 0;
+    for (i, n) in compressed_blocks.iter().enumerate() {
+        if n != "." {
+            let val = n.parse::<i64>().unwrap();
+            checksum += val * i as i64;
+        }
+    }
+    checksum
+}
+
+fn compress_blocks(blocks: Vec<String>) -> Vec<String> {
+    let mut compressed = blocks.clone();
+    let mut next_free_space = blocks.iter().position(|x| x == ".").unwrap();
+
+    for i in 1..blocks.len() {
+        let idx = blocks.len() - i;
+        if blocks[blocks.len() - i].parse::<i32>().is_ok() {
+            compressed.swap(idx, next_free_space);
+            next_free_space = compressed.iter().position(|x| x == ".").unwrap();
+        }
+        if next_free_space > idx - 1 {
+            break;
+        }
+    }
+
+    // TODO: We need to terminate once we are fully compressed
+
+    compressed
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::get_blocks;
+    use crate::{compress_blocks, compute_checksum, get_blocks};
 
     #[test]
     fn generate_blocks() {
         let a = "12345";
-        assert_eq!(get_blocks(a), "0..111....22222");
+        assert_eq!(get_blocks(a).concat(), "0..111....22222");
     }
 
     #[test]
     fn generate_blocks_medium() {
         let a = "2333133121414131402";
-        assert_eq!(get_blocks(a), "00...111...2...333.44.5555.6666.777.888899");
+        assert_eq!(
+            get_blocks(a).concat(),
+            "00...111...2...333.44.5555.6666.777.888899"
+        );
+    }
+
+    #[test]
+    fn test_compression() {
+        let blocks = get_blocks("12345");
+        let compressed = compress_blocks(blocks);
+        assert_eq!(compressed.join(""), "022111222......");
+    }
+
+    #[test]
+    fn test_compression_large() {
+        let blocks = get_blocks("2333133121414131402");
+        let compressed = compress_blocks(blocks);
+        assert_eq!(
+            compressed.join(""),
+            "0099811188827773336446555566.............."
+        );
+    }
+
+    #[test]
+    fn test_checksum() {
+        let blocks = get_blocks("2333133121414131402");
+        let compressed = compress_blocks(blocks);
+        let checksum = compute_checksum(compressed);
+        assert_eq!(checksum, 1928);
     }
 }
